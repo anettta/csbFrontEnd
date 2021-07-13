79aa998538c0a741b83015f4b636abe28b9764ca
Anya <annagapyuk@gmail.com>
2021-07-13 16:42:39 -0400

    created Navigation, Rank, ImageInputForm, and Logo components

--- smart-brain-app/src/App.js
+++ smart-brain-app/src/App.js
@@ -1,14 +1,33 @@
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
-import Navigation from "./components/Navigation";
+import Navigation from "./components/Navigation/Navigation";
+import Logo from "./components/Logo/Logo";
+import ImageInputForm from "./components/ImageInputForm/ImageInputForm";
+import Rank from "./components/Rank/Rank";
+import Particles from "react-particles-js";
+import { Component } from "react";
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 80,
+      density: {
+        enable: true,
+        value_area: 800,
+      },
+    },
+  },
+};
 
 function App() {
   return (
     <div className="App">
+      <Particles className="particles" params={particlesOptions} />
       <Navigation />
-      {/* <Logo />
+      <Logo />
+      <Rank />
       <ImageInputForm />
-      <FaceRecognition /> */}
+      {/* <FaceRecognition /> */}
     </div>
   );
 }
--- smart-brain-app/src/components/Navigation.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Navigation = () => {
-  <nav style={{ display: "flex", justifyContent: "flex-end" }}>
-    <p className="f3 link dim black underline pa3 pointer">Sign Out</p>
-  </nav>;
-};
-
-export default Navigation;
