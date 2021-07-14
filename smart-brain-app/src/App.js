383d221653cd13967734b0a1af0f5699b5d90b80
Anya <annagapyuk@gmail.com>
2021-07-14 14:10:58 -0400

    connected Clarifai API

--- smart-brain-app/src/App.js
+++ smart-brain-app/src/App.js
@@ -4,9 +4,10 @@ import Navigation from "./components/Navigation/Navigation";
 import Logo from "./components/Logo/Logo";
 import ImageInputForm from "./components/ImageInputForm/ImageInputForm";
 import Rank from "./components/Rank/Rank";
+import FaceRecognition from "./components/FaceRecognition/FaceRecognition";
 import Particles from "react-particles-js";
 import { Component } from "react";
-import Clarifai from "clarifai-nodejs-grpc";
+import Clarifai from "clarifai";
 
 const app = new Clarifai.App({
   apiKey: "9953bc2236614c848389cecd9b67cfd5",
@@ -29,50 +30,67 @@ class App extends Component {
     super();
     this.state = {
       input: "",
+      imageUrl: "",
+      box: {},
     };
   }
 
+  calculateFaceLocation = (data) => {};
+
   onInputChange = (event) => {
-    console.log(event.target.value);
+    this.setState({ input: event.target.value });
   };
+
+  // onButtonSubmit = () => {
+  //   this.setState({ imageUrl: this.state.input });
+  //   app.models
+  //     .predict(
+  //       // HEADS UP! Sometimes the Clarifai Models can be down or not working as they are constantly getting updated.
+  //       // A good way to check if the model you are using is up, is to check them on the clarifai website. For example,
+  //       // for the Face Detect Mode: https://www.clarifai.com/models/face-detection
+  //       // If that isn't working, then that means you will have to wait until their servers are back up. Another solution
+  //       // is to use a different version of their model that works like: `c0c0ac362b03416da06ab3fa36fb58e3`
+  //       // so you would change from:
+  //       // .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+  //       // to:
+  //       // .predict('c0c0ac362b03416da06ab3fa36fb58e3', this.state.input)
+  //       "c0c0ac362b03416da06ab3fa36fb58e3",
+  //       this.state.input
+  //     )
+  //     .then((response) => {
+  //       console.log("hi", response);
+  //       if (response) {
+  //         fetch("http://localhost:3000/image", {
+  //           method: "put",
+  //           headers: { "Content-Type": "application/json" },
+  //           body: JSON.stringify({
+  //             id: this.state.user.id,
+  //           }),
+  //         })
+  //           .then((response) => response.json())
+  //           .then((count) => {
+  //             this.setState(Object.assign(this.state.user, { entries: count }));
+  //           });
+  //       }
+  //       this.displayFaceBox(this.calculateFaceLocation(response));
+  //     })
+  //     .catch((err) => console.log(err));
+  // };
 
   onButtonSubmit = () => {
     this.setState({ imageUrl: this.state.input });
     app.models
-      .predict(
-        // HEADS UP! Sometimes the Clarifai Models can be down or not working as they are constantly getting updated.
-        // A good way to check if the model you are using is up, is to check them on the clarifai website. For example,
-        // for the Face Detect Mode: https://www.clarifai.com/models/face-detection
-        // If that isn't working, then that means you will have to wait until their servers are back up. Another solution
-        // is to use a different version of their model that works like: `c0c0ac362b03416da06ab3fa36fb58e3`
-        // so you would change from:
-        // .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
-        // to:
-        // .predict('c0c0ac362b03416da06ab3fa36fb58e3', this.state.input)
-        "c0c0ac362b03416da06ab3fa36fb58e3",
-        this.state.input
-      )
-      .then((response) => {
-        console.log("hi", response);
-        if (response) {
-          fetch("http://localhost:3000/image", {
-            method: "put",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({
-              id: this.state.user.id,
-            }),
-          })
-            .then((response) => response.json())
-            .then((count) => {
-              this.setState(Object.assign(this.state.user, { entries: count }));
-            });
-        }
-        this.displayFaceBox(this.calculateFaceLocation(response));
-      })
-      .catch((err) => console.log(err));
+      .predict("c0c0ac362b03416da06ab3fa36fb58e3", this.state.input)
+      .then(
+        function (response) {
+          console.log(response);
+        },
+        function (err) {}
+      );
   };
 
   render() {
+    const { imageUrl } = this.state;
     return (
       <div className="App">
         <Particles className="particles" params={particlesOptions} />
@@ -83,7 +101,7 @@ class App extends Component {
           onInputChange={this.onInputChange}
           onButtonSubmit={this.onButtonSubmit}
         />
-        {/* <FaceRecognition /> */}
+        <FaceRecognition imageUrl={imageUrl} />
       </div>
     );
   }
