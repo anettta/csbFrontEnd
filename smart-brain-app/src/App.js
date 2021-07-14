22a9e777befb6a5f44b412d30aae0b4a1604719d
Anya <annagapyuk@gmail.com>
2021-07-14 12:29:41 -0400

    removed extra package-lock 2.json

--- smart-brain-app/src/App.js
+++ smart-brain-app/src/App.js
@@ -6,6 +6,11 @@ import ImageInputForm from "./components/ImageInputForm/ImageInputForm";
 import Rank from "./components/Rank/Rank";
 import Particles from "react-particles-js";
 import { Component } from "react";
+import Clarifai from "clarifai-nodejs-grpc";
+
+const app = new Clarifai.App({
+  apiKey: "9953bc2236614c848389cecd9b67cfd5",
+});
 
 const particlesOptions = {
   particles: {
@@ -19,17 +24,69 @@ const particlesOptions = {
   },
 };
 
-function App() {
-  return (
-    <div className="App">
-      <Particles className="particles" params={particlesOptions} />
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageInputForm />
-      {/* <FaceRecognition /> */}
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: "",
+    };
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  };
+
+  onButtonSubmit = () => {
+    this.setState({ imageUrl: this.state.input });
+    app.models
+      .predict(
+        // HEADS UP! Sometimes the Clarifai Models can be down or not working as they are constantly getting updated.
+        // A good way to check if the model you are using is up, is to check them on the clarifai website. For example,
+        // for the Face Detect Mode: https://www.clarifai.com/models/face-detection
+        // If that isn't working, then that means you will have to wait until their servers are back up. Another solution
+        // is to use a different version of their model that works like: `c0c0ac362b03416da06ab3fa36fb58e3`
+        // so you would change from:
+        // .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+        // to:
+        // .predict('c0c0ac362b03416da06ab3fa36fb58e3', this.state.input)
+        "c0c0ac362b03416da06ab3fa36fb58e3",
+        this.state.input
+      )
+      .then((response) => {
+        console.log("hi", response);
+        if (response) {
+          fetch("http://localhost:3000/image", {
+            method: "put",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+              id: this.state.user.id,
+            }),
+          })
+            .then((response) => response.json())
+            .then((count) => {
+              this.setState(Object.assign(this.state.user, { entries: count }));
+            });
+        }
+        this.displayFaceBox(this.calculateFaceLocation(response));
+      })
+      .catch((err) => console.log(err));
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Particles className="particles" params={particlesOptions} />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageInputForm
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onButtonSubmit}
+        />
+        {/* <FaceRecognition /> */}
+      </div>
+    );
+  }
 }
 
 export default App;
--- smart-brain-app/src/components/ImageInputForm/ImageInputForm.js
+++ smart-brain-app/src/components/ImageInputForm/ImageInputForm.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./ImageInputForm.css";
 
-const ImageInputForm = () => {
+const ImageInputForm = ({ onInputChange, onButtonSubmit }) => {
   return (
     <div>
       <p className="f3">
@@ -9,8 +9,15 @@ const ImageInputForm = () => {
       </p>
       <div className="center">
         <div className="form center pa4 br3 shadow-5">
-          <input className="f4 pa2 w-70 center" type="text" />
-          <button className="w-30 grow f4 link ph3 pv2 dib white bg-blue">
+          <input
+            className="f4 pa2 w-70 center"
+            type="tex"
+            onChange={onInputChange}
+          />
+          <button
+            className="w-30 grow f4 link ph3 pv2 dib white bg-blue"
+            onClick={onButtonSubmit}
+          >
             Detect
           </button>
         </div>
