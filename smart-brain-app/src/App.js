317ba42bd3ed6d7c22640ccb8ba9ab275fc2d2aa
Anya <annagapyuk@gmail.com>
2021-07-23 14:47:20 -0400

    clean up

--- smart-brain-app/src/App.js
+++ smart-brain-app/src/App.js
@@ -79,42 +79,6 @@ class App extends Component {
     this.setState({ input: event.target.value });
   };
 
-  // onButtonSubmit = () => {
-  //   this.setState({ imageUrl: this.state.input });
-  //   app.models
-  //     .predict(
-  //       // HEADS UP! Sometimes the Clarifai Models can be down or not working as they are constantly getting updated.
-  //       // A good way to check if the model you are using is up, is to check them on the clarifai website. For example,
-  //       // for the Face Detect Mode: https://www.clarifai.com/models/face-detection
-  //       // If that isn't working, then that means you will have to wait until their servers are back up. Another solution
-  //       // is to use a different version of their model that works like: `c0c0ac362b03416da06ab3fa36fb58e3`
-  //       // so you would change from:
-  //       // .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
-  //       // to:
-  //       // .predict('c0c0ac362b03416da06ab3fa36fb58e3', this.state.input)
-  //       "c0c0ac362b03416da06ab3fa36fb58e3",
-  //       this.state.input
-  //     )
-  //     .then((response) => {
-  //       console.log("hi", response);
-  //       if (response) {
-  //         fetch("http://localhost:3000/image", {
-  //           method: "put",
-  //           headers: { "Content-Type": "application/json" },
-  //           body: JSON.stringify({
-  //             id: this.state.user.id,
-  //           }),
-  //         })
-  //           .then((response) => response.json())
-  //           .then((count) => {
-  //             this.setState(Object.assign(this.state.user, { entries: count }));
-  //           });
-  //       }
-  //       this.displayFaceBox(this.calculateFaceLocation(response));
-  //     })
-  //     .catch((err) => console.log(err));
-  // };
-
   onButtonSubmit = () => {
     this.setState({ imageUrl: this.state.input });
     fetch("https://ancient-oasis-15667.herokuapp.com/imageurl", {
