0f857beb9e7e2cdcadbe391b14a0e2c220cae28b
Anya <annagapyuk@gmail.com>
2021-07-23 13:52:02 -0400

    deploy

--- smart-brain-app/src/App.js
+++ smart-brain-app/src/App.js
@@ -117,7 +117,7 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({ imageUrl: this.state.input });
-    fetch("http://localhost:3001/imageurl", {
+    fetch("https://ancient-oasis-15667.herokuapp.com/imageurl", {
       method: "post",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
@@ -127,7 +127,7 @@ class App extends Component {
       .then((response) => response.json())
       .then((response) => {
         if (response) {
-          fetch("http://localhost:3001/image", {
+          fetch("https://ancient-oasis-15667.herokuapp.com/image", {
             method: "put",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({
